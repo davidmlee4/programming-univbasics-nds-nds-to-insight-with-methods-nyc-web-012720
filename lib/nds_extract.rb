372820c704aa36f021b50c996081dd21bc68c5b1
Muhan Lee <davidmlee4@gmail.com>
2020-01-26 21:49:04 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -21,6 +21,11 @@ end
 def directors_totals(nds)
 pp nds
 result = {}
+name_index = 0
+while name_index < nds.length do
+  director = nds[name_index] #this is the same set of hashes that was iterated over in the
+  result[director[:name]] = gross_for_director(director)                      #first order method so we plug in that method here 
+end
 
 
 end
