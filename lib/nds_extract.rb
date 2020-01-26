cede00e114f3851667693b1e318713318fd64444
Muhan Lee <davidmlee4@gmail.com>
2020-01-26 21:36:43 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -4,13 +4,7 @@ require 'directors_database'
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
-  total = 0
-  movie_index = 0
-  while movie_index < director_data[:movies].length do
-    total += director_data[:movies][movie_index][:worldwide_gross]
-    movie_index += 1
-  end
-  total
+pp director_data
 end
 
 # Write a method that, given an NDS creates a new Hash
